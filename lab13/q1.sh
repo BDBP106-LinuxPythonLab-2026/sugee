5b96369be1c5de9226a9877aad8496e44b5af209
sugeeghoderao <sugeeghoderao@gmail.com>
2026-08-27 18:44:19 +0530

    lab13 work

---
+++ lab13/q1.sh
@@ -0,0 +1,15 @@
+Python is a high-level, interpreted programming language
+that is widely used for web development, data science, automation,
+artificial intelligence, and more.
+It was created by Guido van Rossum in 1991 and
+has grown into one of the most popular programming languages in the world.
+Key Features of Python
+Readable and simple syntax - Python code often looks like plain English,
+making it beginner-friendly.
+Interpreted language - You don’t need to compile Python;
+you can run your code directly.
+Cross-platform - Works on Windows, macOS, Linux, and many other systems.
+Extensive libraries - Python has built-in modules and
+third-party packages (via PyPI) for tasks like math,
+data analysis, networking, and visualization.
+Versatile use cases - From small scripts to large-scale applications.
