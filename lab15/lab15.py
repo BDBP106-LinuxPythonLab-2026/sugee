ad4caaa91c0947ea88ee9ded395993639e6440b2
sugeeghoderao <sugeeghoderao@gmail.com>
2026-09-03 18:40:45 +0530

    Add files via upload

---
+++ lab15/lab15.py
@@ -0,0 +1,35 @@
+#arithematic oper
+import math
+from unittest import result
+
+from PIL.ImImagePlugin import number
+#question3
+"""k=int(input("enter a number:"))
+n=int(input("enter a number:"))
+if n>0:
+    a=k+n
+    b=k-n
+    c=k*n
+    d=k**n
+    e=k/n
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)"""
+
+#question 4
+"""import math
+a=float(input("Enter the number (a): "))
+b=float(input("Enter the number (b): "))
+c=float(input("Enter the number (c): "))
+d= (b*b) - (4*a*c)
+e=math.sqrt(d)
+result1= (-b+e) / (2*a)
+result2= (-b-e) / (2*a)
+print ("Result 1= ", result1)
+print ("Result 2= ", result2)"""
+
+
+
+
