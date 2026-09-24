da6a47dced3a1369c20db71fb5d53a7033c48eb4
sugeeghoderao <sugeeghoderao@gmail.com>
2026-09-24 15:47:39 +0530

    lab16work

---
+++ lab16/lab16.py
@@ -0,0 +1,97 @@
+#Q1
+#string=input("enter a string:")
+#print(string)
+
+#Q3
+# #age=int(input("enter your age"))
+#if age>18:
+    #print("you are eligible for voting")
+#else:
+    #print("you are not eligible for voting")
+
+#Q4
+##import math
+#angle=int(input("enter any angle in degrees: "))
+#ang=(math.radians(angle))
+#sin=(math.sin(ang))
+#cos=(math.cos(ang))
+#tan=(math.tan(ang))
+#cosec=(math.cos(ang))
+#sec=(math.sin(ang))
+#cot=(math.tan(ang))
+#print(sin)
+#print(cos)
+#print(tan)
+#print(cosec)
+#print(sec)
+#print(cot)
+
+#Q13
+#import math
+#NUMBER=int(input("Enter a number: "))
+#factorial=1
+#for i in range(NUMBER,1, -1):
+#    factorial=factorial*i
+#print(factorial)
+
+#Q7
+# num=int(input("Enter a number: "))
+# original=num
+# rev=0
+# while num > 0:
+#      n=num % 10
+#      rev=rev*10+n
+#      num=num//10
+# if original==rev:
+#          print(original, "is a palindrome")
+# else:
+#          print(original, "is not a palindrome")
+
+#Q6
+# NUMBER=int(input("enter a number:"))
+# sum=0
+# while NUMBER>0:
+#     digit=NUMBER%10
+#     sum=sum+digit
+#     NUMBER=NUMBER//10
+# print(sum)
+
+#Q8
+# x=float(input("Enter a point: "))
+# y=float(input("Enter another point: "))
+# if x>0 and y>0:
+#      print("the points lie in 1st quadrant")
+# elif x<0 and y>0:
+#      print("the points lie in 2nd quadrant")
+# elif x<0 and y<0:
+#     print("the points lie in 3rd quadrant")
+# elif x>0 and y<0:
+#     print("the points lie in 4th quadrant")
+# elif x==0:
+#     print("the point is on y axis")
+# elif y==0:
+#     print("the point is on x axis")
+# else:
+#    print("error")
+
+#Q10
+# NUMBER=int(input("enter a number: "))
+# if (NUMBER%2==0):
+#     print("even")
+# else:
+#     print("odd")
+
+#Q15
+# import math
+# X1=float(input("enter point 1: "))
+# X2=float(input("enter point 2: "))
+# Y1=float(input("enter point 3: "))
+# Y2=float(input("enter point 4: "))
+#
+# distance=math.sqrt(((X2-X1)**2) +((Y2-Y1)**2))
+#
+# print(distance)
+
+
+
+
